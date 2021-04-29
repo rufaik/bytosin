216a9df9eecfb27afeee1f6ebb7d017dca6d9e56
System Administrator <rufai@hotmail.co.uk>
2021-04-29 13:27:41 +0100

    final7

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -61,7 +61,7 @@ const Contact = () => {
 
               <p><b>contact</b></p>
 
-              <p><b style={{textDecoration:"none"}}>E:</b> <span style={{textDecoration:"underline"}} >info@bytosin.com</span></p>
+              <p><b style={{textDecoration:"none"}}>e:</b> <span style={{textDecoration:"underline"}} >info@bytosin.com</span></p>
 
               <p><b>please note:</b> all photos are taken on a google pixel 4</p>
         </div>
