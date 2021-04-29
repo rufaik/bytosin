bd58962348a25d75153d2581652afa7f78095b99
System Administrator <rufai@hotmail.co.uk>
2021-04-29 13:26:41 +0100

    final6

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -63,7 +63,7 @@ const Contact = () => {
 
               <p><b style={{textDecoration:"none"}}>E:</b> <span style={{textDecoration:"underline"}} >info@bytosin.com</span></p>
 
-              <p><b>please note:</b> all photos are taken on a Google Pixel 4.</p>
+              <p><b>please note:</b> all photos are taken on a google pixel 4</p>
         </div>
         <a href="https://instagram.com/tosinalabi?igshid=zz626ehwo1lz">
              <p className='instatext'> @tosinalabi</p>
